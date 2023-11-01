e9d5ab47971d0752eecbd507b1e9cb1274fe0e42
unknown <bilza32611@gmail.com>
2023-11-02 01:37:24 +0500

    alhamdullah

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -9,13 +9,65 @@ import DdStatus from "./DdStatus.svelte";
 import Summary from '$lib/appComp/Summary.svelte';
 
 let questions;
+let totalChapterQuestions;
+let totalChapterUnlocked;
+let totalChapterFill;
+let totalChapterLocked;
+let totalChapterFinal;
 
 let selectedChapter = 1;
 let selectedStatus = 'fill';
+let showAllStatus = true;
 
+function toggleShowAllStatus(){
+showAllStatus = !showAllStatus;
+return showAllStatus;
+}
 let isLogin = false;
 let isAdmin = false;
-let totalCount = false;
+// let totalCount = false;
+let printableQuestions;
+$:{
+  selectedChapter;
+  if(questions){
+  const tcq  = questions.filter(question => 
+      question.chapter ===     selectedChapter);
+  totalChapterQuestions = tcq.length;
+  /////////////////////////////////////
+  const tcu  = questions.filter(question => 
+      question.chapter === selectedChapter && question.status === 'unlocked');
+  totalChapterUnlocked = tcu.length;
+  /////////////////////////////////////
+//   
+// 
+// 
+  const dsssaw  = questions.filter(question => 
+      question.chapter === selectedChapter && question.status === 'fill');
+  totalChapterFill = dsssaw.length;
+  /////////////////////////////////////
+  const ssdsddsd  = questions.filter(question => 
+      question.chapter === selectedChapter && question.status === 'locked');
+  totalChapterLocked = ssdsddsd.length;
+  /////////////////////////////////////
+  const kipojhg  = questions.filter(question => 
+  question.chapter === selectedChapter && question.status === 'final');
+  totalChapterFinal = kipojhg.length;
+
+  }
+}
+$: {
+  // debugger;
+  selectedChapter;
+  showAllStatus;
+  if(questions){
+    if (showAllStatus){
+    printableQuestions = questions.filter(question => question.chapter === selectedChapter);
+    }else {
+    printableQuestions = questions.filter(question => question.chapter === selectedChapter && question.status === selectedStatus);
+    }
+  }
+}
+
 
 function setStatus(n){
 selectedStatus = n;
@@ -46,7 +98,7 @@ onMount(async () => {
               toast.push(data.message);
             }
   } catch (e) {
-       toast.push('failed to load');
+      //  toast.push('failed to load');
   }      
 });
 function getUrl(question){
@@ -71,12 +123,16 @@ return url;
 <div class="p-4 m-1 border-2 border-white rounded-lg">
 <Dd {setChapter} {selectedChapter}/>
   <br/>
-<DdStatus {setStatus} {selectedStatus} />  
+<DdStatus {setStatus} {selectedStatus}  {toggleShowAllStatus} {showAllStatus} {totalChapterQuestions} {totalChapterUnlocked}
+{totalChapterFill}
+{totalChapterLocked}
+{totalChapterFinal}
+/>  
   <br/>
 <div class="flex flex-wrap justify-center w-full">
-    {#each questions as question}
-        {#if question.chapter == selectedChapter}
-        {#if question.status == selectedStatus || selectedStatus == 'all' }
+    {#each printableQuestions as question}
+        <!-- {#if question.chapter == selectedChapter} -->
+        <!-- {#if question.status == selectedStatus || selectedStatus == 'all' } -->
         <div class='w-3/12'>
             <Card
             title = {`Ch:${question.chapter} Ex:${question.partNo.exercise} Q:${question.partNo.questionNo}`}
@@ -86,8 +142,8 @@ return url;
             <div class="bg-gray-800 rounded-md m-1 p-1 text-xs px-2">{question.status}</div>
             </Card>
         </div>
-        {/if}
-        {/if}
+        <!-- {/if} -->
+        <!-- {/if} -->
     {/each}
 
 </div>
--- src/routes/admin/DdStatus.svelte
+++ src/routes/admin/DdStatus.svelte
@@ -1,7 +1,19 @@
 <script>
 //@ts-nocheck
+import { Icons } from "$lib/util";
+
 export let setStatus;
 export let selectedStatus;
+export let toggleShowAllStatus;
+export let showAllStatus;
+
+export let totalChapterQuestions;
+export let totalChapterUnlocked;
+export let totalChapterFill;
+export let totalChapterLocked;
+export let totalChapterFinal;
+
+
 </script>
 
 <div class="flex justify-center">
@@ -11,12 +23,22 @@ export let selectedStatus;
 </div>
 
 <div class="flex justify-center w-full flex-wrap ">
-<button on:click={()=>setStatus('all')}  class={`p-1 m-1 rounded-lg  px-2 ${selectedStatus== 'all'? 'bg-gray-400':'bg-gray-600' }`}>All</button>
+<button on:click={toggleShowAllStatus}  class={`p-1 m-1 rounded-lg  px-2 ${showAllStatus ? 'bg-green-800':'bg-stone-700' }`}>
+All ( {totalChapterQuestions} )
+{#if showAllStatus }
+{Icons.BULB}
+{:else}
+{Icons.CROSS}
+{/if }
+</button>
 
-<button on:click={()=>setStatus('unlocked')}  class={`p-1 m-1 rounded-lg  px-2 ${selectedStatus== 'unlocked'? 'bg-gray-400':'bg-gray-600' }`}>Unlocked</button>
-<button on:click={()=>setStatus('fill')}  class={`p-1 m-1 rounded-lg  px-2 ${selectedStatus== 'fill'? 'bg-gray-400':'bg-gray-600' }`}>Fill</button>
-<button on:click={()=>setStatus('locked')}  class={`p-1 m-1 rounded-lg  px-2 ${selectedStatus== 'locked'  ? 'bg-gray-400':'bg-gray-600' }`}>Locked</button>
-<button on:click={()=>setStatus('final')}  class={`p-1 m-1 rounded-lg  px-2 ${selectedStatus== 'final' ? 'bg-gray-400':'bg-gray-600' }`}>Final</button>
+<button on:click={()=>setStatus('unlocked')}  class={`p-1 m-1 rounded-lg  px-2 ${(selectedStatus == 'unlocked' && showAllStatus ==false)? 'bg-gray-400':'bg-gray-600' }`}>Unlocked ( {totalChapterUnlocked} )</button>
+
+<button on:click={()=>setStatus('fill')}  class={`p-1 m-1 rounded-lg  px-2 ${(selectedStatus== 'fill'  && showAllStatus ==false)? 'bg-gray-400':'bg-gray-600' }`}>Fill ( {totalChapterFill} )</button>
+
+<button on:click={()=>setStatus('locked')}  class={`p-1 m-1 rounded-lg  px-2 ${(selectedStatus== 'locked' && showAllStatus ==false)  ? 'bg-gray-400':'bg-gray-600' }`}>Locked ( {totalChapterLocked} )</button>
+
+<button on:click={()=>setStatus('final')}  class={`p-1 m-1 rounded-lg  px-2 ${(selectedStatus== 'final'  && showAllStatus ==false) ? 'bg-gray-400':'bg-gray-600' }`}>Final ( {totalChapterFinal} )</button>
 </div>
 
 <!-- </div> -->
