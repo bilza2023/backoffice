1ae8e44e35ac910fc51b381b2e8ba0711511baf4
unknown <bilza32611@gmail.com>
2023-11-04 06:30:48 +0500

    pre next step Schema made

--- src/routes/preplayer/BaseComp.svelte
+++ src/routes/preplayer/BaseComp.svelte
@@ -7,6 +7,6 @@
 export let pulse=0;
 export let compData={}; //code and template st
 export let theme;
-export let globals={};
+// export let globals={}; // no its a bad bas idea... if you get unlimited input it means this app is open for CHANE which should not be the case it should only be open for EXTENTION.
 
 </script>
--- src/routes/preplayer/fn/getNewComp.js
+++ src/routes/preplayer/fn/getNewComp.js
@@ -1,5 +1,11 @@
 //4-nov-2024
 //@ts-nocheck
+/***
+- No data from Presentation and Ample data from component internal. 
+- dont use callbacks in comps since they are just display items.its not an app its presentation.
+
+
+ */
 export default function getNewComp(){
 
 return {
@@ -14,7 +20,19 @@ return {
     //-This can be changed from this place only --manually
     version:'0.0.0',
     //--this is the object that is different for each slide and hence is Mixed type.
-    code : {},
+    // ---it has extra filed too for more data {name : '' , value : ''}
+    // -- i  have fol means to send data to a comp
+        // - content :string : the default way
+        // - content : JSON.stringify() to pack more data
+        // - extra: Add data to the extra array (name,value pair)
+        // - items : add more number of items as in table each cell is an item with its own content and extras.
+    items : [
+        {name : '', content:'' , showAt :0, hideAt:null , entryStyle:null , existStyle:null , extra : []}
+    ],
+    //---this is some data (key,value pair) that the COMPONENT (not the presentation) has of its own.for example in a table the number of rows and columns are not associated with any item rather with slide so we can add those here.
+    compExtra : [
+        {name : '' , value : ''}
+    ]
 
 };
 
