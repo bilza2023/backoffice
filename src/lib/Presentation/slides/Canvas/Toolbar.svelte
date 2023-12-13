af239f2863262aba9f48547f49fafcfc13af8b86
unknown <bilza32611@gmail.com>
2023-12-14 03:47:26 +0500

    In the name of Allah

--- src/lib/Presentation/slides/Canvas/Toolbar.svelte
+++ src/lib/Presentation/slides/Canvas/Toolbar.svelte
@@ -7,24 +7,28 @@ export let items;
 $: points = $pointsStore;
 let txt = '';
 
+function undo(){
+ items.pop();
+ items = [...items];
+}
 function textDraw(){
     // debugger;
     if (points.length < 1) {
     toast.push('points missing');
     return;
     }
-    items.push({name: 'drawText',showAt :  0 , itemExtra : {x:points[0].x, y:points[0].y, text:txt, fontSize : 24, textColor : 'white'}});
+    items.push({name: 'drawText',showAt :  0 , extra : {x:points[0].x, y:points[0].y, text:txt, fontSize : 24, textColor : 'white'}});
     items = [...items];
     txt = '';
     pointsStore.set([]);
 }
 function rayDraw(){
-    // debugger;
     if (points.length < 2) {
     toast.push('points missing');
     return;
     }
-    items.push({name: 'drawRay',showAt :  0 , itemExtra : {x1:points[0].x, y1:points[0].y, x2:points[1].x, y2:points[1].y, lineWidth : 2, lineColor : 'white'}});
+    items.push({name: 'drawRay',showAt :  0 , extra : {
+    x1:parseInt(points[0].x), y1:parseInt(points[0].y), x2:parseInt(points[1].x), y2:parseInt(points[1].y), lineWidth : 2, lineColor : 'white'}});
     items = [...items];
     pointsStore.set([]);
 }
@@ -32,6 +36,11 @@ function rayDraw(){
 </script>
 
 <div class='flex justify-start border-2 border-gray-500'>
+<button 
+class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 px-1 rounded m-1" on:click={undo}>
+Undo
+</button>
+
 <button 
 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 px-1 rounded m-1" on:click={()=>pointsStore.set([])}>
   Pts {`${points.length}`}
