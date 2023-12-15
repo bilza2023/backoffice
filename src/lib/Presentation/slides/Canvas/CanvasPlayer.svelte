54d412e76b7e8eba93301c11d60e31dd8514700e
unknown <bilza32611@gmail.com>
2023-12-15 12:47:28 +0500

    fixed the editor->page and Left bar from git

--- src/lib/Presentation/slides/Canvas/CanvasPlayer.svelte
+++ src/lib/Presentation/slides/Canvas/CanvasPlayer.svelte
@@ -1,60 +1,98 @@
 <script>
     //@ts-nocheck
   import { onMount } from '$lib/util';
-  import { afterUpdate } from 'svelte';
+
+  export let items;
+  export let slideExtra = [];
+  export let theme;
+   import { afterUpdate } from 'svelte';
   import drawLine from './fn/drawLine';
   import drawGrid from './fn/drawGrid';
   import drawRay from './fn/drawRay';
   import drawText from './fn/drawText';
-  import getPoint from './fn/getPoint';
-  import ToolBar from './Toolbar.svelte';
-  export let items;
-  export let slideExtra = [];
-  export let theme;
+  import drawTriangle from './fn/drawTriangle';
+  import drawRectangle from './fn/drawRectangle';
+  import drawPoly from './fn/drawPoly';
+  import drawCircle from './fn/drawCircle';
+  import drawEllipse from './fn/drawEllipse';
+  import drawHline from './fn/drawHline';
+  import drawPoint from './fn/drawPoint';
+  import drawPointWText from './fn/drawPointWText';
 
+  
   let canvas;
   let ctx;
 
   onMount(() => {
+   debugger;
     ctx = canvas.getContext('2d');
-    canvas.width = 1200;
-    canvas.height = 400;
+    updateCanvasSize(); // Call the function initially
+    window.addEventListener('resize', updateCanvasSize);
   });
    
-
-
+  function updateCanvasSize() {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    console.log( 'x' , canvas.width, 'Y' , canvas.height  );
+} 
+  
   afterUpdate(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawGrid(ctx, canvas, 1);
 
-    drawRay(canvas,ctx,20,3,20,99);
-    drawText(canvas, ctx,21,10,'Y',24);
-    drawText(canvas, ctx,21,98,'Y\'',24);
-
-    drawRay(canvas,ctx,10,90,90,90);
-    drawText(canvas, ctx,12,89,'X\'',24);
-    drawText(canvas, ctx,85,89,'X',24);
-
-    drawRay(canvas,ctx,21,88,90,10);
-
-    drawText(canvas, ctx,17,77,'1.6',24);
-    drawText(canvas, ctx,17,60,'3.2',24);
-    drawText(canvas, ctx,17,48,'4.8',24);
-    drawText(canvas, ctx,17,25,'6.4',24);
-    // drawText(canvas, ctx,17,10,'8.0',24);
-
-    drawText(canvas, ctx,35,96,'1',24);
-    drawText(canvas, ctx,50,96,'2',24);
-    drawText(canvas, ctx,65,96,'3',24);
-    drawText(canvas, ctx,80,96,'4',24);
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        switch (item.name) {
+            case 'drawRay':
+            //  debugger;
+                drawRay(canvas,ctx,item.extra.x1,item.extra.y1,item.extra.x2,item.extra.y2,item.extra.lineWidth,item.extra.lineColor);
+            break;
+            case 'drawHline':
+            //  debugger;
+                drawHline(canvas,ctx,item.extra.x1,item.extra.y1,item.extra.x2,item.extra.y2,item.extra.lineWidth,item.extra.lineColor);
+            break;
+            case 'drawText':
+                drawText(canvas,ctx,item.extra.x, item.extra.y,item.extra.text, item.extra.fontSize, item.extra.textColor);
+            break;
+            case 'drawLine':
+                drawLine(canvas,ctx,item.extra.x1,item.extra.y1,item.extra.x2,item.extra.y2,item.extra.lineWidth,item.extra.lineColor);
+            break;
+            case 'drawTriangle':
+                drawTriangle(canvas,ctx,item.extra.x1,item.extra.y1,item.extra.x2,item.extra.y2,item.extra.x3,item.extra.y3,item.extra.x4,item.extra.y4,item.extra.fillColor,item.extra.opacity,item.extra.filled);
+            break;
+            case 'drawRectangle':
+                drawRectangle(canvas,ctx,item.extra.x1,item.extra.y1,item.extra.x2,item.extra.y2,item.extra.fillColor,item.extra.opacity,item.extra.filled);
+            break;
+            case 'drawPoly':
+                drawPoly(canvas,ctx,item.extra.points,item.extra.fillColor,item.extra.opacity,item.extra.filled);
+            break;
+            case 'drawCircle':
+                drawCircle(canvas,ctx,item.extra.x1,item.extra.y1,item.extra.x2,item.extra.y2,item.extra.fillColor,item.extra.opacity,item.extra.filled);
+            break;
+            case 'drawPoint':
+                drawPoint(canvas,ctx,item.extra.points,item.extra.width,item.extra.fillColor,item.extra.opacity);
+            break;
+            case 'drawPointWText':
+                drawPointWText(canvas,ctx,item.extra.x1,item.extra.y1,item.extra.text,item.extra.width,item.extra.color,item.extra.colorText);
+            break;
+            case 'drawEllipse':
+                drawEllipse(canvas,ctx,item.extra.x1,item.extra.y1,item.extra.x2,item.extra.y2,item.extra.x3,item.extra.y3,item.extra.fillColor,item.extra.opacity,item.extra.filled);
+            break;
+            case 'drawGrid':
+            drawGrid(canvas,ctx);
+            break;       
+            default:
+                break;
+        }
+        
+    }
+  
   });
 
 </script>
 
 
-<div class='flex flex-col w-full justify-center'>
-<ToolBar />
-<canvas bind:this={canvas} on:click={ e =>getPoint(e,canvas)} ></canvas>
+<div class='flex flex-col w-full justify-center p-0 m-0'>
+  <canvas width="1200" height="400" bind:this={canvas}   />
 </div>
 
 
