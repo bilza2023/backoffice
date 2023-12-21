a265b573fadbcc3b440675f933baf80a2b0f77f2
unknown <bilza32611@gmail.com>
2023-12-21 15:53:54 +0500

    working on moveing canvas out of main code

--- src/lib/Presentation/slides/Canvas/fn/percToX.js
+++
@@ -1,9 +0,0 @@
-//@ts-nocheck
-
- 
-export default function percToX(canvas,x) {
-    const canvasWidth = canvas.width;
-    // Calculate the pixel value based on the percentage
-    const pixelX = Math.round((canvasWidth / 100) * x);
-    return pixelX;
-}
--- src/lib/Presentation/slides/Canvas_attempt2/store.js
+++
@@ -1,6 +0,0 @@
-
-import { writable } from 'svelte/store';
-
-// Creating a writable store for points
-export const pointsStore = writable([]);
-export const selectedItemStore = writable(null);
