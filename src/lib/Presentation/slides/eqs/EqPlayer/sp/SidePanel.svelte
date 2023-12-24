060e39b86a38d57e61e04308c893d6b28f0b4f16
unknown <bilza32611@gmail.com>
2023-12-24 07:40:20 +0500

    side planel simplified

--- src/lib/Presentation/slides/eqs/EqPlayer/sp/SidePanel.svelte
+++ src/lib/Presentation/slides/eqs/EqPlayer/sp/SidePanel.svelte
@@ -1,39 +1,19 @@
 <script>
 //@ts-nocheck
 import ImgCodeTxt from "./ImgCodeTxt.svelte";
-export let pulse;
-export let items;
-export let closeFs=()=>{};
-export let spORfs;
+export let currentItem;
 
-$:{
-   pulse;
-   items = [...items];
-}
-
-function isFocus(item){
-   // debugger;
-   if (pulse >= item.extra.startTime && pulse < item.extra.endTime ){
-       return true; 
-   }else {
-      return false;  
-   }
-}
 </script> 
-{#if spORfs=='fs'}
-<button on:click={closeFs} class='text-xs text-yellow-600'>close fullscreen</button>
-{/if}
 
 <div class='justify-center sticky top-4   line-clamp-4 text-yellow-300'>
-{#each items as item}
- {#if  isFocus(item) }    
+
+ {#if  currentItem }    
         <div class='w-full  rounded-md m-1 p-1 bg-stone-600 text-center'>
-        {#if item && item.extra[spORfs].length > 0 }    
-                {#each item.extra[spORfs] as eq}    
+        {#if  currentItem.extra.sp.length > 0 }    
+                {#each currentItem.extra.sp as eq}    
                 <ImgCodeTxt {eq} />        
                 {/each}
         {/if}
         </div>
  {/if}
-{/each}        
 </div>
