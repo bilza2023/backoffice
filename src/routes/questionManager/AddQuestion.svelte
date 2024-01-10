58e0c473bb81951023e265518e0fc47db43b5c0b
unknown <bilza32611@gmail.com>
2024-01-10 21:48:40 +0500

    config

--- src/routes/questionManager/AddQuestion.svelte
+++ src/routes/questionManager/AddQuestion.svelte
@@ -5,9 +5,8 @@ import { toast,BASE_URL } from "$lib/util";
 let board = 'FBISE';
 let classNo =9;
 let chapter= "1";
-let isSpecial= "false";
 let exercise ='1.1';
-let questionNo =99;
+let questionNo =999;
 let part=9;
 let name='';
 
@@ -27,12 +26,13 @@ function checkName(isSpecial,name){
 
 async function addQuestion(){
 
-if (!checkName(isSpecial,name)){
-    toast.push('Special Questions must have a name');
-return;
-}
-
+// if (!checkName(isSpecial,name)){
+//     toast.push('Special Questions must have a name');
+// return;
+// }
+if(name && name !== ''){
 name = convertToUrlFriendlyName(name);
+}
 // debugger;
   let token = localStorage.getItem("token");
     const response = await fetch( `${BASE_URL}/q/add_question` ,{
@@ -62,38 +62,22 @@ name = convertToUrlFriendlyName(name);
 <h1 class='mx-auto'>Add Question</h1>
 
 <!--Board-->
-<div class='flex justify-around  border-2 border-gray-600 p-1 m-1 rounded-sm'>
+<!-- <div class='flex justify-around  border-2 border-gray-600 p-1 m-1 rounded-sm'>
     <div class='text-sm text-center border-2 border-yellow-700 rounded-md p-1 w-4/12'>Board</div>
     <div class='text-sm text-center border-2 border-yellow-700 rounded-md p-1 w-8/12'>fbise</div>
-</div>
+</div> -->
 
 <!--Class no-->
 <div class='flex justify-around  border-2 border-gray-600 p-1 m-1 rounded-sm'>
     <div class='text-sm text-center border-2 border-yellow-700 rounded-md p-1 w-4/12'>Class</div>
-    <div class='text-sm text-center border-2 border-yellow-700 rounded-md p-1 w-8/12'>9</div>
+    <input type='number' class='text-sm text-center border-2 border-yellow-700 rounded-md p-1 w-8/12 bg-gray-800 text-white' bind:value={classNo} />
 </div>
 
 <!--Chapter-->
 <div class='flex justify-around  border-2 border-gray-600 p-1 m-1 rounded-sm'>
     <div class='text-sm text-center border-2 border-yellow-700 rounded-md p-1 w-4/12'>Chapter</div>
-    <select class='text-sm text-center border-2 border-yellow-700 rounded-md p-1 w-8/12 bg-gray-800 text-white' bind:value={chapter}>
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-    <option value="7">7</option>
-    <option value="8">8</option>
-    <option value="9">9</option>
-    <option value="10">10</option>
-    <option value="11">11</option>
-    <option value="12">12</option>
-    <option value="13">13</option>
-    <option value="14">14</option>
-    <option value="15">15</option>
-    <option value="16">16</option>
-    </select>
+    <input type='number' class='text-sm text-center border-2 border-yellow-700 rounded-md p-1 w-8/12 bg-gray-800 text-white' bind:value={chapter} />
+    
 </div>
 
 
@@ -113,16 +97,6 @@ name = convertToUrlFriendlyName(name);
 <div class='flex justify-around  border-2 border-gray-600 p-1 m-1 rounded-sm'>
     <div class='text-sm text-center border-2 border-yellow-700 rounded-md p-1 w-4/12'>Part No</div>
     <input class='text-sm text-center bg-gray-800 text-white border-2 border-yellow-700 rounded-md p-1 w-8/12' type='number' bind:value={part}/>
-</div>
-
-
-<!--isSpecial-->
-<div class='flex justify-around  border-2 border-gray-600 p-1 m-1 rounded-sm'>
-    <div class='text-sm text-center border-2 border-yellow-700 rounded-md p-1 w-4/12'>isSpecial</div>
-    <select class='text-sm text-center border-2 border-yellow-700 rounded-md p-1 w-8/12 bg-gray-800 text-white' bind:value={isSpecial}>
-    <option value="false">False</option>
-    <option value="true">True</option>
-    </select>
 </div>
 
 <!--Name-->
