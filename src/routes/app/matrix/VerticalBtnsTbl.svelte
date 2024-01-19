a831477bfa56bb6e8c182fb62c8b723b0cb86fb6
unknown <bilza32611@gmail.com>
2024-01-20 01:20:37 +0500

    first app added

---
+++ src/routes/app/matrix/VerticalBtnsTbl.svelte
@@ -0,0 +1,26 @@
+<script>
+//@ts-nocheck
+
+export let addRow;
+export let delRow;
+export let addCol;
+export let delCol;
+</script>
+
+
+<div class="flex flex-col bg-zinc-900 rounded-md p-2 text-white ">
+
+
+    <div class='flex justify-center'>
+    <div class='w-4/12 text-xs p-1 m-1 bg-gray-700'>Row</div>
+    <button on:click={addRow} class="w-4/12 text-xs p-1 m-1 bg-gray-700">+</button>
+    <button on:click={delRow} class="w-4/12 text-xs p-1 m-1 bg-red-900">-</button>
+    </div>
+    
+    <div class='flex justify-center'>
+    <div class='w-4/12 text-xs p-1 m-1 bg-gray-700'>Col</div>
+    <button on:click={addCol} class="w-4/12 text-xs p-1 m-1 bg-gray-700">+</button>
+    <button on:click={delCol} class="w-4/12 text-xs p-1 m-1 bg-red-900">-</button>
+    </div>
+
+</div>
