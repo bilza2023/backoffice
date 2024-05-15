a9520a7256e7f9dd010e53c62bcf353b533940bc
Bilal Tariq <bilza2023@gmail.com>
2024-05-15 15:34:07 +0500

    ok

--- src/lib/util/config.js
+++ src/lib/util/config.js
@@ -1,8 +1,8 @@
 
 
-    // export const API_URL = 'http://localhost:5000';
+    export const API_URL = 'http://localhost:5000';
     
     //==This is the new taleem_api using bilza2023 Atlas mongodb
-   export const API_URL = 'https://taleem.help/api';  
+//    export const API_URL = 'https://taleem.help/api';  
 
 export const RESOURCE_URL = 'https://taleem-media.blr1.cdn.digitaloceanspaces.com'; // 
