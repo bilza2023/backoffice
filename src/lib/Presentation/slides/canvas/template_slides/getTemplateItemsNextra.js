3a5642fa037219c90ea99ae46ca9b3bd8216e3c5
Bilal Tariq <bilza2023@gmail.com>
2024-06-02 16:36:50 +0500

    Alhamdullah added new slide-templates very very successful-- now finally may be the last piece of the puzzle SYSTEM-IMAGES  .

--- src/lib/Presentation/slides/canvas/template_slides/getTemplateItemsNextra.js
+++ src/lib/Presentation/slides/canvas/template_slides/getTemplateItemsNextra.js
@@ -2,8 +2,11 @@
 //@ts-nocheck
 import {imgW4lbl} from "./imgW4lbl";
 import {imgW2lbl} from "./imgW2lbl";
+import {imgW3lbl} from "./imgW3lbl";
 import {jt1} from "./jt1";
 import {rnl} from "./rnl";
+import {h2li} from "./h2li";
+import {h3li} from "./h3li";
 
 export default function getTemplateItemsNextra(template_name){
 let ret = null;
@@ -29,6 +32,24 @@ let ret = null;
     return {
         "items" : rnl.items,
         "extra" : rnl.extra,
+    }
+    }
+    if(template_name == "h2li"){
+    return {
+        "items" : h2li.items,
+        "extra" : h2li.extra,
+    }
+    }
+    if(template_name == "h3li"){
+    return {
+        "items" : h3li.items,
+        "extra" : h3li.extra,
+    }
+    }
+    if(template_name == "imgW3lbl"){
+    return {
+        "items" : imgW3lbl.items,
+        "extra" : imgW3lbl.extra,
     }
     }
     
--- src/routes/editor/toolbar/TemplatesDlg.svelte
+++ src/routes/editor/toolbar/TemplatesDlg.svelte
@@ -8,10 +8,12 @@ export let addTemplate;
 
 <div class='flex flex-wrap  bg-gray-900 border-2 border-white'>
 
-    <NavBtn2 title='test' icon={Icons.TEMPLATE }  clk={()=>addTemplate('test')} />            
     <NavBtn2 title='jt1' icon={Icons.TEMPLATE }  clk={()=>addTemplate('jt1')} />            
     <NavBtn2 title='rnl' icon={Icons.TEMPLATE }  clk={()=>addTemplate('rnl')} />            
-    <NavBtn2 title='imgW4lbl' icon={Icons.TEMPLATE }  clk={()=>addTemplate('imgW4lbl')} />            
-    <NavBtn2 title='imgW2lbl' icon={Icons.TEMPLATE }  clk={()=>addTemplate('imgW2lbl')} />            
+        <NavBtn2 title='imgW2lbl' icon={Icons.TEMPLATE }  clk={()=>addTemplate('imgW2lbl')} />            
+        <NavBtn2 title='imgW3lbl' icon={Icons.TEMPLATE }  clk={()=>addTemplate('imgW3lbl')} />            
+            <NavBtn2 title='imgW4lbl' icon={Icons.TEMPLATE }  clk={()=>addTemplate('imgW4lbl')} />            
+    <NavBtn2 title='h2li' icon={Icons.TEMPLATE }  clk={()=>addTemplate('h2li')} />            
+    <NavBtn2 title='h3li' icon={Icons.TEMPLATE }  clk={()=>addTemplate('h3li')} />            
  
 </div>        
