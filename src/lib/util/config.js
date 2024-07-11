53684bfd4a85ef0d45b4428172739e4654719dff
Bilal Tariq <bilza2023@gmail.com>
2024-07-11 12:06:56 +0500

    reboot

--- src/lib/util/config.js
+++ src/lib/util/config.js
@@ -2,11 +2,9 @@
 
 // export const API_URL = 'http://localhost:5000';
     
-    //==This is the new taleem_api using bilza2023 Atlas mongodb
 export const API_URL = 'https://taleem.help/api';  
 
-//--why was i not using CDN all this time?
-// export const SOUND_FILE_PATH = 'https://taleem-media.blr1.digitaloceanspaces.com/sound/';  
+
 export const SOUND_FILE_PATH = 'https://taleem-media.blr1.cdn.digitaloceanspaces.com/sound/';  
 
 export const RESOURCE_URL = 'https://taleem-media.blr1.cdn.digitaloceanspaces.com'; // 
