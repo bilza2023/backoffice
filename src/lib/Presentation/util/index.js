fcf50325c814a9701de6a033d884ba9d93d512cd
bilza2023 <bilza2023@gmail.com>
2024-09-24 03:58:08 +0500

    more clean up in presentation removed useless items from util

--- src/lib/Presentation/util/index.js
+++ src/lib/Presentation/util/index.js
@@ -5,24 +5,23 @@ import { toast } from '@zerodevx/svelte-toast';
 import { fade } from 'svelte/transition';
 import { get } from 'svelte/store';
 import { goto } from '$app/navigation';
-import checkToken from './checkToken';
 import updateStoreArray from './updateStoreArray';
-import checkAdminToken from './checkAdminToken';
 import convertEqsToSlide from './convertEqToSlide.js';
 import { API_URL,RESOURCE_URL,SOUND_FILE_PATH } from './config';
-import { isLoginStore,isAdminStore } from './appStore';
+
 import Icons from './icons';
 import ajaxPost from './ajaxPost';
 import ajaxPostData from './ajaxPostData';
 import ajaxPostMsg from './ajaxPostMsg';
 import ajaxGet from './ajaxGet';
 import uuid from './uuid';
-// import chqLogin from './chqLogin';
 
             export {
-            isLoginStore,isAdminStore,
+
+                
             API_URL,
             RESOURCE_URL,
+            SOUND_FILE_PATH,
                     onMount,
                     toast,
                     fade,
@@ -33,13 +32,10 @@ import uuid from './uuid';
                     goto,
                     browser,
                     Icons,
-                    checkToken,
-                    checkAdminToken,
                     ajaxPost,
                     ajaxPostData,
                     ajaxPostMsg,
-                    ajaxGet,
-                    SOUND_FILE_PATH
+                    ajaxGet
             }
 
 /*<div in:fade={{ delay: 300 }} out:fade={{ delay: 300 }} */
