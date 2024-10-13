d2bdd1433f0c20f6492e661de5a65082e375d0ad
bilza2023 <bilza2023@gmail.com>
2024-10-13 22:15:21 +0500

    This is very important point since the Presentation library is now seperate in taleemPresentation thus the local Presentation in Backoffice is being deprecated

---
+++ src/routes/static/+page.svelte
@@ -0,0 +1,11 @@
+
+<script>
+    import {PresentationPlayerBlob} from "taleempresentation";
+    import {questionData} from './questionData.js';
+    import audioData from "./fbise9math2024_ch_1_ex_1.1_q_1.js" 
+</script>
+
+<h1>Presentation</h1>
+
+<PresentationPlayerBlob tcode='fbise9math' {questionData} {audioData}/>
+        
---
+++ src/routes/static/questionData.js
@@ -0,0 +1,628 @@
+export const questionData= {
+    "_id": {
+      "$oid": "6646aee20f8a3c642cd26c19"
+    },
+    "tcode": "fbise9math",
+    "chapter": 1,
+    "exercise": "1.1",
+    "filename": "fbise9math2024_ch_1_ex_1.1_q_1",
+    "questionNo": 1,
+    "part": 0,
+    "name": "",
+    "questionType": "paid",
+    "status": "final",
+    "slides": [
+      {
+        "startTime": 0,
+        "endTime": 6,
+        "type": "canvas",
+        "template": "",
+        "items": [
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "translate": true,
+              "name": "Chapter",
+              "command": "text",
+              "text": {
+                "initialValue": "Chapter 1 Ex 1.1 Q 1",
+                "setCommands": []
+              },
+              "x": {
+                "initialValue": 100,
+                "setCommands": []
+              },
+              "y": {
+                "initialValue": 125,
+                "setCommands": []
+              },
+              "color": {
+                "initialValue": "#FAE500",
+                "setCommands": []
+              },
+              "font": "80px Arial",
+              "shadowOffsetX": 12,
+              "shadowOffsetY": 12,
+              "shadowColor": "#9D850B",
+              "useShowHide": true,
+              "showAt": 0,
+              "globalAlpha": {
+                "initialValue": 1,
+                "setCommands": []
+              },
+              "shadowBlur": 12,
+              "gap": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "dash": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "setCommands": [],
+              "fontSize": {
+                "initialValue": "80",
+                "setCommands": []
+              }
+            },
+            "arr": [],
+            "_id": {
+              "$oid": "6646b7cc16c0a014bfd5b593"
+            }
+          }
+        ],
+        "slideExtra": [],
+        "extra": {
+          "backgroundColor": "#03260A",
+          "canvasWidth": 1000,
+          "canvasHeight": 360,
+          "cellHeight": 25,
+          "cellWidth": 25,
+          "xFactor": 0,
+          "yFactor": 0,
+          "showGrid": false,
+          "gridLineWidth": 1,
+          "gridLineColor": "#363C34",
+          "bgGlobalAlpha": 1
+        },
+        "_id": {
+          "$oid": "6646b7cc16c0a014bfd5b592"
+        }
+      },
+      {
+        "startTime": 6,
+        "endTime": 56,
+        "type": "canvas",
+        "template": "",
+        "items": [
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "translate": true,
+              "name": "7/8 Greater than 0 But smaller than 1",
+              "command": "text",
+              "text": {
+                "initialValue": "7/8 Greater than 0 But smaller than 1",
+                "setCommands": []
+              },
+              "x": {
+                "initialValue": 250,
+                "setCommands": []
+              },
+              "y": {
+                "initialValue": 250,
+                "setCommands": []
+              },
+              "color": {
+                "initialValue": "#ed8e07",
+                "setCommands": []
+              },
+              "font": "35px Arial",
+              "shadowOffsetX": 0,
+              "shadowOffsetY": 0,
+              "shadowColor": "gray",
+              "useShowHide": false,
+              "showAt": 34,
+              "globalAlpha": {
+                "initialValue": 1,
+                "setCommands": []
+              },
+              "shadowBlur": 0,
+              "gap": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "dash": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "setCommands": [],
+              "fontSize": {
+                "initialValue": "35",
+                "setCommands": []
+              }
+            },
+            "arr": [],
+            "_id": {
+              "$oid": "664aa5c516c0a014bfd5c123"
+            }
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "translate": true,
+              "name": "7/8",
+              "command": "text",
+              "text": {
+                "initialValue": "7/8 = Pick 7 out of a total of 8 pieces",
+                "setCommands": []
+              },
+              "x": {
+                "initialValue": 250,
+                "setCommands": []
+              },
+              "y": {
+                "initialValue": 150,
+                "setCommands": []
+              },
+              "color": {
+                "initialValue": "#ed8e07",
+                "setCommands": []
+              },
+              "font": "35px Arial",
+              "shadowOffsetX": 0,
+              "shadowOffsetY": 0,
+              "shadowColor": "gray",
+              "useShowHide": false,
+              "showAt": 17,
+              "globalAlpha": {
+                "initialValue": 1,
+                "setCommands": []
+              },
+              "shadowBlur": 0,
+              "gap": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "dash": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "setCommands": [],
+              "fontSize": {
+                "initialValue": "35",
+                "setCommands": []
+              }
+            },
+            "arr": [],
+            "_id": {
+              "$oid": "664aa5c516c0a014bfd5c124"
+            }
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "translate": true,
+              "name": "jt_80108257",
+              "command": "text",
+              "text": {
+                "initialValue": "Plot 7/8 on Real Line",
+                "setCommands": []
+              },
+              "x": {
+                "initialValue": 287.5,
+                "setCommands": []
+              },
+              "y": {
+                "initialValue": 25,
+                "setCommands": []
+              },
+              "color": {
+                "initialValue": "#FAE500",
+                "setCommands": []
+              },
+              "font": "50px Arial",
+              "shadowOffsetX": 12,
+              "shadowOffsetY": 12,
+              "shadowColor": "#9D850B",
+              "useShowHide": false,
+              "showAt": 0,
+              "globalAlpha": {
+                "initialValue": 1,
+                "setCommands": []
+              },
+              "shadowBlur": 12,
+              "gap": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "dash": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "setCommands": [],
+              "fontSize": {
+                "initialValue": "50",
+                "setCommands": []
+              }
+            },
+            "arr": [],
+            "_id": {
+              "$oid": "664aa5c516c0a014bfd5c125"
+            }
+          }
+        ],
+        "slideExtra": [],
+        "extra": {
+          "backgroundColor": "#03260A",
+          "canvasWidth": 1000,
+          "canvasHeight": 360,
+          "cellHeight": 25,
+          "cellWidth": 25,
+          "xFactor": 0,
+          "yFactor": 0,
+          "showGrid": false,
+          "gridLineWidth": 1,
+          "gridLineColor": "#363C34",
+          "bgGlobalAlpha": 1
+        },
+        "_id": {
+          "$oid": "664aa5c516c0a014bfd5c122"
+        }
+      },
+      {
+        "startTime": 56,
+        "endTime": 105,
+        "type": "canvas",
+        "template": "",
+        "items": [
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "translate": true,
+              "name": "jt_16984845",
+              "command": "text",
+              "text": {
+                "initialValue": "7/8",
+                "setCommands": []
+              },
+              "x": {
+                "initialValue": 550,
+                "setCommands": []
+              },
+              "y": {
+                "initialValue": 300,
+                "setCommands": []
+              },
+              "color": {
+                "initialValue": "#FAE500",
+                "setCommands": []
+              },
+              "font": "40px Arial",
+              "shadowOffsetX": 12,
+              "shadowOffsetY": 12,
+              "shadowColor": "#9D850B",
+              "useShowHide": true,
+              "showAt": 96,
+              "globalAlpha": {
+                "initialValue": 1,
+                "setCommands": []
+              },
+              "shadowBlur": 12,
+              "gap": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "dash": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "setCommands": [],
+              "fontSize": {
+                "initialValue": "40",
+                "setCommands": []
+              }
+            },
+            "arr": [],
+            "_id": {
+              "$oid": "664aa5c516c0a014bfd5c128"
+            }
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "translate": true,
+              "name": "ray-final",
+              "command": "ray",
+              "x0": {
+                "initialValue": 550,
+                "setCommands": []
+              },
+              "y0": {
+                "initialValue": 300,
+                "setCommands": []
+              },
+              "x1": {
+                "initialValue": 475,
+                "setCommands": []
+              },
+              "y1": {
+                "initialValue": 200,
+                "setCommands": []
+              },
+              "lineColor": "#eb3b0f",
+              "lineWidth": {
+                "initialValue": 2,
+                "setCommands": []
+              },
+              "arrowWidth": {
+                "initialValue": 8,
+                "setCommands": []
+              },
+              "arrowHeight": {
+                "initialValue": 12,
+                "setCommands": []
+              },
+              "startArrow": true,
+              "endArrow": false,
+              "globalAlpha": {
+                "initialValue": 1,
+                "setCommands": []
+              },
+              "useShowHide": true,
+              "showAt": 94,
+              "dash": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "gap": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "color": {
+                "initialValue": "#fc0303",
+                "setCommands": []
+              },
+              "setCommands": []
+            },
+            "arr": [],
+            "_id": {
+              "$oid": "664aa5c516c0a014bfd5c129"
+            }
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "translate": true,
+              "name": "repeatDot_99228366",
+              "command": "repeatDot",
+              "numberOfDots": 7,
+              "initialX": 175,
+              "initialY": 200,
+              "xFactor": 50,
+              "yFactor": 0,
+              "width": 11,
+              "useShowHide": true,
+              "showAt": 76,
+              "color": {
+                "initialValue": "#014af4",
+                "setCommands": []
+              },
+              "globalAlpha": {
+                "initialValue": 1,
+                "setCommands": []
+              },
+              "gap": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "dash": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "setCommands": []
+            },
+            "arr": [],
+            "_id": {
+              "$oid": "664aa5c516c0a014bfd5c12a"
+            }
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "translate": true,
+              "name": "jt",
+              "command": "text",
+              "text": {
+                "initialValue": "Show 7/8 on Real Number Line",
+                "setCommands": []
+              },
+              "x": {
+                "initialValue": 275,
+                "setCommands": []
+              },
+              "y": {
+                "initialValue": 25,
+                "setCommands": []
+              },
+              "color": {
+                "initialValue": "#FAE500",
+                "setCommands": []
+              },
+              "font": "30px Arial",
+              "shadowOffsetX": 12,
+              "shadowOffsetY": 12,
+              "shadowColor": "#9D850B",
+              "useShowHide": false,
+              "showAt": 0,
+              "globalAlpha": {
+                "initialValue": 1,
+                "setCommands": []
+              },
+              "shadowBlur": 12,
+              "gap": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "dash": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "setCommands": [],
+              "fontSize": {
+                "initialValue": "30",
+                "setCommands": []
+              }
+            },
+            "arr": [],
+            "_id": {
+              "$oid": "664aa5c516c0a014bfd5c12b"
+            }
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "translate": true,
+              "name": "repeatDot_28499639",
+              "command": "repeatDot",
+              "numberOfDots": 4,
+              "initialX": 125,
+              "initialY": 200,
+              "xFactor": 400,
+              "yFactor": 0,
+              "width": 18,
+              "useShowHide": false,
+              "showAt": 0,
+              "color": {
+                "initialValue": "#ff0000",
+                "setCommands": []
+              },
+              "globalAlpha": {
+                "initialValue": 1,
+                "setCommands": []
+              },
+              "gap": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "dash": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "setCommands": []
+            },
+            "arr": [],
+            "_id": {
+              "$oid": "664aa5c516c0a014bfd5c12d"
+            }
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "translate": true,
+              "name": "ray",
+              "command": "ray",
+              "x0": {
+                "initialValue": 50,
+                "setCommands": []
+              },
+              "y0": {
+                "initialValue": 200,
+                "setCommands": []
+              },
+              "x1": {
+                "initialValue": 950,
+                "setCommands": []
+              },
+              "y1": {
+                "initialValue": 200,
+                "setCommands": []
+              },
+              "lineColor": "#ecfc03",
+              "lineWidth": {
+                "initialValue": 2,
+                "setCommands": []
+              },
+              "arrowWidth": {
+                "initialValue": 8,
+                "setCommands": []
+              },
+              "arrowHeight": {
+                "initialValue": 12,
+                "setCommands": []
+              },
+              "startArrow": true,
+              "endArrow": true,
+              "globalAlpha": {
+                "initialValue": 1,
+                "setCommands": []
+              },
+              "useShowHide": false,
+              "showAt": 0,
+              "dash": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "gap": {
+                "initialValue": 0,
+                "setCommands": []
+              },
+              "color": {
+                "initialValue": "#e1f104",
+                "setCommands": []
+              },
+              "setCommands": []
+            },
+            "arr": [],
+            "_id": {
+              "$oid": "664aa5c516c0a014bfd5c12e"
+            }
+          }
+        ],
+        "slideExtra": [],
+        "extra": {
+          "backgroundColor": "#03260A",
+          "canvasWidth": 1000,
+          "canvasHeight": 360,
+          "cellHeight": 25,
+          "cellWidth": 25,
+          "xFactor": 0,
+          "yFactor": 0,
+          "showGrid": true,
+          "gridLineWidth": 1,
+          "gridLineColor": "#363C34",
+          "bgGlobalAlpha": 1
+        },
+        "_id": {
+          "$oid": "664aa5c516c0a014bfd5c126"
+        }
+      }
+    ],
+    "tags": [],
+    "sortOrder": 1.1,
+    "teacherComments": "",
+    "adminComments": "fixed",
+    "__v": 0,
+    "soundFileType": "opus",
+    "version": "0.1"
+  }
